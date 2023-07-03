dde9202c3b78abefbce40397da9add5de6f8ab30
Labaynecss <labaynecss@gmail.com>
2023-07-03 14:48:15 +0800

    adding properties in mongo db

--- models/User.js
+++ models/User.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
     username: { type: String, unique: true },
     password: String,
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
-const UserModel = mongoose.model('User', UserSchema);
+const UserModel = mongoose.model("User", UserSchema);
 module.exports = UserModel;
